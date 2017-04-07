4262d23133e9bcd93f511ed9b4664e0be1323352
Starlynne <lewis.start0905@gmail.com>
2017-04-07 13:20:40 -0700

    Add logic to add all costs together for total

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,54 @@
 //Business Logic
-function Pizza (size, crust, cheese, cost, toppings) {
+function Pizza (size, crust, cheese, costs, toppings) {
   this.size = size;
   this.crust = crust;
   this.cheese = cheese;
   this.toppings = toppings;
-  this.cost = cost;
+  this.costs = costs;
 }
+var myPizza = new Pizza();
 
-
-var choiceArr = function(){
-  $("input:checkbox[name=top]:checked").next
-}
-
-
-
+ function getTotal(total, num){
+   return total + num;
+  }
 
 
 //UI Logic
 $(document).ready(function(){
   $("#sizeAndCrust").submit(function(event){
     event.preventDefault();
-    var size = $("#sizeChoice option:selected").text();
-    var sizeCost = parseInt($("#sizeChoice").val());
-    var crust = $("#crustChoice option:selected").text();
-    var crustCost = parseInt($("#crustChoice").val());
-    var cheese = $("#cheeseChoice option:selected").text();
-    var cheeseCost = parseInt($("#cheeseChoice").val());
-    var basicCost = sizeCost + crustCost + cheeseCost;
-    var myPizza = new Pizza(size, crust, cheese, basicCost);
-    console.log(myPizza);
+
+     var size = $("#sizeChoice option:selected").text();
+     var sizeCost = parseInt($("#sizeChoice").val());
+     var crust = $("#crustChoice option:selected").text();
+     var crustCost = parseInt($("#crustChoice").val());
+     var cheese = $("#cheeseChoice option:selected").text();
+     var cheeseCost = parseInt($("#cheeseChoice").val());
+     var basicCost = sizeCost + crustCost + cheeseCost;
+     myPizza = new Pizza (size, crust, cheese);
+     myPizza.costs = (sizeCost + crustCost + cheeseCost);
+     
+     console.log(myPizza);
   });
+
   $("#toppings").submit(function(event){
     event.preventDefault();
-    var toppingCost = [];
+     var toppingCost = [];
     $("input:checkbox[name=top]:checked").each(function(){
       toppingCost.push(parseInt($(this).val()));
     });
-
+    console.log(toppingCost);
     var toppingChoices =
     $("input:checkbox[name=top]:checked + label").map(function() {
       return $(this).text();
     }).get()
+    toppingCost = toppingCost.reduce(getTotal);
+    myPizza.toppings = toppingChoices;
+    myPizza.costs = (myPizza.costs + toppingCost);
 
-      console.log(toppingChoices);
-      console.log(toppingCost);
+    console.log(myPizza.costs);
+
   });
+
+
 });
