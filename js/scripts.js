6cfd240b8f618fe9145e307a672bbe2eee94ce9c
Starlynne <lewis.start0905@gmail.com>
2017-04-07 11:53:19 -0700

    Add form for toppings, logic to gather user input and value

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 //Business Logic
+function Pizza (size, crust, cheese, cost, toppings) {
+  this.size = size;
+  this.crust = crust;
+  this.cheese = cheese;
+  this.toppings = toppings;
+  this.cost = cost;
+}
 
 
-
-
+var choiceArr = function(){
+  $("input:checkbox[name=top]:checked").next
+}
 
 
 
@@ -16,6 +24,25 @@ $(document).ready(function(){
     var sizeCost = parseInt($("#sizeChoice").val());
     var crust = $("#crustChoice option:selected").text();
     var crustCost = parseInt($("#crustChoice").val());
-    alert(size + " " + crust + ", $" + (sizeCost + crustCost));
+    var cheese = $("#cheeseChoice option:selected").text();
+    var cheeseCost = parseInt($("#cheeseChoice").val());
+    var basicCost = sizeCost + crustCost + cheeseCost;
+    var myPizza = new Pizza(size, crust, cheese, basicCost);
+    console.log(myPizza);
+  });
+  $("#toppings").submit(function(event){
+    event.preventDefault();
+    var toppingCost = [];
+    $("input:checkbox[name=top]:checked").each(function(){
+      toppingCost.push(parseInt($(this).val()));
+    });
+
+    var toppingChoices =
+    $("input:checkbox[name=top]:checked + label").map(function() {
+      return $(this).text();
+    }).get()
+
+      console.log(toppingChoices);
+      console.log(toppingCost);
   });
 });
